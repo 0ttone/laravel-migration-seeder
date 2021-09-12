971bedb18cf2caeff5b127de155e88a60827eba2
MyUsername <emanuele.marini37@gmail.com>
2021-09-12 12:17:09 +0200

    step two

---
+++ resources/views/trip.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>migration-seeder-Trip</title>
+</head>
+<body>
+      <h1>view trip che sarà associata al TripController</h1>
+</body>
+</html>
